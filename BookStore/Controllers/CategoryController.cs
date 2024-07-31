9dd0ebbafbc43d6ebf8ea50baeb35ff2696f0e75
Sairash <sairash0323@gmail.com>
2024-07-31 10:51:27 +0545

    Add project files.

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BookStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            Category? CategoryFromDb = _db.categories.Find(id);
+            if(CategoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(CategoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, DisplayOrder = 1, Name = "Sci Fi" },
+                new Category { Id = 2, DisplayOrder = 2, Name = "Self Help" },
+                new Category { Id = 3, DisplayOrder = 3, Name = "Education" }
+                );
+        }
+    }
+}
